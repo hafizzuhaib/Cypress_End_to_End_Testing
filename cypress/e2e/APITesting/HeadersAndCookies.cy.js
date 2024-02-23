033d391b9e34c375ce0e234600990d7cb203122d
Hafiz Zuhaib <hafizzuhaibulhussan@gmail.com>
2024-02-23 18:13:17 +0500

    Error Fixes

--- cypress/e2e/APITesting/HeadersAndCookies.cy.js
+++ cypress/e2e/APITesting/HeadersAndCookies.cy.js
@@ -1,29 +1,26 @@
 describe("API Testing",()=>{
-    before(() => {
-        cy.visit("http://localhost:6535");
-    });
     let authToken=null;
-    before("Creating Access Token",()=>{
-        cy.request({
-            method: 'POST',
-            url:'https://simple-books-api.glitch.me/api-clients/',
-            Headers:{'Content-Type':'application/json'},
-            body:{
-                clientName: Math.random().toString(5).substring(2),
-                clientEmail: Math.random().toString(5).substring(2)+"gmail.com"
-        }
-        })
-        .then((response)=>{
-                authToken=response.body.accessToken;
-        })
-
-    before("Creating Access Token",()=>{
+        before("Creating Access Token",()=>{
+            cy.request({
+                    method: 'POST',
+                    url:'https://simple-books-api.glitch.me/api-clients/',
+                    headers:{'Content-Type':'application/json'},
+                    body:{
+                        clientName: Math.random().toString(5).substring(2),
+                        clientEmail: Math.random().toString(5).substring(2)+"@gmail.com"
+                    }
+            })
+            .then((response)=>{
+                    authToken=response.body.accessToken;
+            });
+        });
+        before("Creating Access Token",()=>{
             cy.request({
                 method: 'POST',
-                url:'https://simple-books-api.glitch.me/orders',
-                Headers:{
+                url:'https://simple-books-api.glitch.me/orders/',
+                headers:{
                     'Content-Type':'application/json',
-                    'Authorization':'Bearer' + authToken
+                    'Authorization':'Bearer '+authToken
                 },
                 body:{
                     
@@ -32,27 +29,26 @@ describe("API Testing",()=>{
                 }
             })
             .then((response)=>{
-                expect(response.status).equal(201);
-                expect(response.body.created).equal(true);   
+                expect(response.status).to.eq(201);
+                expect(response.body.created).to.eq(true);   
             });
         });
-
         it("Fetching Orders",()=>{
             cy.request({
                 method: 'GET',
-                url:'https://simple-books-api.glitch.me/orders',
-                Headers:{
+                url: 'https://simple-books-api.glitch.me/orders/',
+                headers: {
                     'Content-Type':'application/json',
-                    'Authorization':'Bearer' + authToken,
+                    'Authorization':'Bearer '+authToken,
                 },
                 cookies:{
-                    'cookieName': 'mycookie'
+                    'cookieName': 'myCookie'
                 },                
             })
             .then((response)=>{
-                expect(response.status).equal(200);
+                expect(response.status).to.eq(200);
                 expect(response.body).has.length(1);   
             });
         })
-    })
+
 })
