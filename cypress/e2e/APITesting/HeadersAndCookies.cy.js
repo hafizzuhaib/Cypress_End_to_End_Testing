ab48f002c463cd507249dcb35a642c0ce1c5e2f4
Hafiz Zuhaib <hafizzuhaibulhussan@gmail.com>
2024-02-23 16:08:54 +0500

    Get Order with Bearer token

--- cypress/e2e/APITesting/HeadersAndCookies.cy.js
+++ cypress/e2e/APITesting/HeadersAndCookies.cy.js
@@ -1,4 +1,7 @@
 describe("API Testing",()=>{
+    before(() => {
+        cy.visit("http://localhost:6535");
+    });
     let authToken=null;
     before("Creating Access Token",()=>{
         cy.request({
@@ -14,30 +17,42 @@ describe("API Testing",()=>{
                 authToken=response.body.accessToken;
         })
 
-
-        before("Creating Access Token",()=>{
+    before("Creating Access Token",()=>{
             cy.request({
                 method: 'POST',
                 url:'https://simple-books-api.glitch.me/orders',
-                Headers:{'Content-Type':'application/json'},
+                Headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':'Bearer' + authToken
+                },
                 body:{
-                    clientName: Math.random().toString(5).substring(2),
-                    clientEmail: Math.random().toString(5).substring(2)+"gmail.com"
-            }
+                    
+                    "bookId": 1,
+                    "customerName": "AsadZuhaib"         
+                }
             })
             .then((response)=>{
-                    authToken=response.body.accessToken;
+                expect(response.status).equal(201);
+                expect(response.body.created).equal(true);   
+            });
+        });
+
+        it("Fetching Orders",()=>{
+            cy.request({
+                method: 'GET',
+                url:'https://simple-books-api.glitch.me/orders',
+                Headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':'Bearer' + authToken,
+                },
+                cookies:{
+                    'cookieName': 'mycookie'
+                },                
             })
+            .then((response)=>{
+                expect(response.status).equal(200);
+                expect(response.body).has.length(1);   
+            });
+        })
     })
-
-
-
-
-
-
-
-
-
-
-
 })
